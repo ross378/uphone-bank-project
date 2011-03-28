7ed590500b9cf280ca00f169c500e3a38b0c250d
kjsoloho <kjsoloho@e94fa02d-4b64-ac12-59de-c8328ce7a654>
2011-03-28 03:37:07 +0000

    冲突

--- cellphonebank_webservices/src/com/ultrawise/android/bank/Enum/EOperation.java
+++ cellphonebank_webservices/src/com/ultrawise/android/bank/Enum/EOperation.java
@@ -1,15 +1,29 @@
 package com.ultrawise.android.bank.Enum;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.ultrawise.log.Log;
 
 public enum EOperation {
+	/* 系统 */
+	GET_ACC_TYPE("0101"), GET_ACC_WITH_NICKNAME("0103"), ADD_ACC("0104"), GET_ACC(
+			"0105"), GET_ACC_TYPE_ALL("0106"), GET_ACC_TYPE_ON_CREDITCARD(
+			"0107"), GET_COME_HISTORY("0108"), GET_COST("0109"), GET_CREDIT_CARD(
+			"0110"), GET_EXTRA_CODE("0111"), GET_LIST_HISTORY("0112"), GET_NET(
+			"0113"), GET_NET_ADDRESS("0114"), GET_OPERATOR("0115"), GET_PAYMENT_HISTORY(
+			"0116"), GET_PAYMENT_INFO("0117"), GET_PAYMENT_NAME("0118"), GET_PAYMENT_NAME_ON_MANA(
+			"0119"), GET_PRE_ACC("0120"), GET_SEL_SERVICE_NAME("0121"), GET_SEL_SERVICE_NAME_BY_USER_ID(
+			"0122"), GET_USED_CREDIT_CARD("0123"), LOGIN("0124"), SET_PRE_ACC(
+			"0125"), UPDATE_PAYMENT_STATE("0126"),
+	/* 系统 */
+
 	/* 储蓄卡 */
-	GET_ACC_INFO("0601"),
-	GET_TRARGETPHONEINFO("0301"),GET_TRANSFEACT("0302"),GET_LISTQUERYINFO("0303"),
-	GET_COMEQUERYINFO("0304"),GET_SET_DETAIL("0305"),GET_PAYMENTHISINFO("0306"),
-	GET_RECHARGE("0307"),GET_PAYMENTINFO("0308");
+	GET_ACC_INFO("0601"), GET_TRARGETPHONEINFO("0301"), GET_TRANSFEACT("0302"), GET_LISTQUERYINFO(
+			"0303"), GET_COMEQUERYINFO("0304"), GET_SET_DETAIL("0305"), GET_PAYMENTHISINFO(
+			"0306"), GET_RECHARGE("0307"), GET_PAYMENTINFO("0308");
 	/* 储蓄卡 */
-	
+
 	private final String mNum;
 
 	private EOperation(String num) {
@@ -17,16 +31,31 @@ public enum EOperation {
 	}
 
 	public static EOperation getEoperation(String num) {
+		List<EOperation> list = new ArrayList<EOperation>();
 
 		for (EOperation e : EOperation.values()) {
 			if (e.mNum.equals(num)) {
-				return e;
+				list.add(e);
 			}
+
 		}
-		Log.getInstance().getLogger().error(
-				"Enum EOperation isn't find,pelse check the param from client");
-		throw new NullPointerException(
-				"I can't find Enum EOperation,so it is null");
+		/* 检测是否为空 */
+		if (list.size() == 0) {
+			Log
+					.getInstance()
+					.getLogger()
+					.error(
+							"Enum EOperation isn't find,pelse check the param from client");
+			throw new NullPointerException(
+					"I can't find Enum EOperation,so it is null");
+		}
+
+		/* 如果有多个相同操作号，就报错。PS: 没有找到更合适的控制不能重复添加，只能延迟到代码执行的时候才知道 */
+		if (list.size() > 1) {
+			throw new IllegalArgumentException(
+					"It's not client's fault.Operation has been here! don't input again");
+		}
+		return list.get(0);
 	}
 
 	public static String getOperNum(EOperation e) {
--- cellphonebank_webservices/src/com/ultrawise/android/bank/webservices/Action.java
+++ cellphonebank_webservices/src/com/ultrawise/android/bank/webservices/Action.java
@@ -41,8 +41,9 @@ public class Action {
 	}
 
 	/**
-	 * 取得账户类型，不包括定期账户
+	 * 功能号:0101
 	 * 
+	 * @author hosolo
 	 * @return
 	 */
 	public JSONObject performGetAccType() {
@@ -50,8 +51,9 @@ public class Action {
 	}
 
 	/**
-	 * 取得所有账户信息，带有别名
+	 * 功能号：0103
 	 * 
+	 * @author hosolo
 	 * @param userId
 	 * @param accType
 	 * @param accState
@@ -67,19 +69,291 @@ public class Action {
 	/**
 	 * 传入账户号，取得账户管理所需要得账户信息
 	 * 
+	 * @author 王亭
 	 * @param account
 	 * @return 账户信息
 	 */
 	public JSONObject performGetAccInfo(String account) {
-		// Map<String, String> map = mAccount.getAccInfoOnAccMana(account);
 		JSONObject jsonObj = new JSONObject();
 		jsonObj = Helper.wrapUp(mCurrentDepositt.getAccInfo(account));
 		return jsonObj;
 	}
+
+	/**
+	 * 功能号：0104
+	 * 
+	 * @author hosolo
+	 * @param userId
+	 * @param accNo
+	 * @param accType
+	 * @param accNickName
+	 * @param accPwd
+	 * @return
+	 */
+	public JSONObject performAddAcc(String userId, String accNo,
+			String accType, String accNickName, String accPwd) {
+		return Helper.wrapUp(mAccSystem.addAcc(userId, accNo, accType,
+				accNickName, accPwd));
+	}
+
+	/**
+	 * 0105
+	 * 
+	 * @author hosolo
+	 * @param userId
+	 * @param accType
+	 * @param accState
+	 * @return
+	 */
+	public JSONObject performGetAcc(String userId, String accType,
+			AccState accState) {
+		return Helper.wrapUp(mAccSystem.getAcc(userId, accType, accState));
+	}
+
+	/**
+	 * 0106
+	 * 
+	 * @author hosolo
+	 * @return
+	 */
+	public JSONObject performGetAccTypeAll() {
+		return Helper.wrapUp(mAccSystem.getAccTypeAll());
+	}
+
+	/**
+	 * 0107
+	 * 
+	 * @author hosolo
+	 * @return
+	 */
+	public JSONObject performGetAccTypeOnCreditCard() {
+		return Helper.wrapUp(mAccSystem.getAccTypeOnCreditCard());
+	}
+
+	/**
+	 * 0108
+	 * 
+	 * @author hosolo
+	 * @param userId
+	 * @param startDate
+	 * @param endDate
+	 * @return
+	 */
+	public JSONObject performGetComeHistory(String userId, String startDate,
+			String endDate) {
+		return Helper.wrapUp(mAccSystem.getComeHistory(userId, startDate,
+				endDate));
+	}
+
+	/**
+	 * 0109
+	 * 
+	 * @author hosolo
+	 * @param costType
+	 * @return
+	 */
+	public JSONObject performGetCost(String costType) {
+		return Helper.wrapUp(mAccSystem.getCost(costType));
+	}
+
+	/**
+	 * 0110
+	 * 
+	 * @author hosolo
+	 * @param userId
+	 * @return
+	 */
+	public JSONObject performGetCreditCard(String userId) {
+		return Helper.wrapUp(mAccSystem.getCreditCard(userId));
+	}
+
+	/**
+	 * 0111
+	 * 
+	 * @author hosolo
+	 * @return
+	 */
+	public JSONObject performExtraCode() {
+		return Helper.wrapUp(mAccSystem.getExtraCode());
+	}
+
+	/**
+	 * 0112
+	 * 
+	 * @author hosolo
+	 * @param userId
+	 * @param startDate
+	 * @param endDate
+	 * @return
+	 */
+	public JSONObject performGetListHistory(String userId, String startDate,
+			String endDate) {
+		return Helper.wrapUp(mAccSystem.getListHistory(userId, startDate,
+				endDate));
+	}
+
+	/**
+	 * 0113
+	 * 
+	 * @author hosolo
+	 * @return
+	 */
+	public JSONObject performGetNet() {
+		return Helper.wrapUp(mAccSystem.getNet());
+	}
+
+	/**
+	 * 0114
+	 * 
+	 * @author hosolo
+	 * @param net
+	 * @return
+	 */
+	public JSONObject performGetNetAddress(String net) {
+		return Helper.wrapUp(mAccSystem.getNetAddress(net));
+	}
+
+	/**
+	 * 0115
+	 * 
+	 * @author hosolo
+	 * @param paymentName
+	 * @return
+	 */
+	public JSONObject performGetOperator(String paymentName) {
+		return Helper.wrapUp(mAccSystem.getOperator(paymentName));
+	}
+
+	/**
+	 * 0116
+	 * 
+	 * @author hosolo
+	 * @param userId
+	 * @param startDate
+	 * @param endDate
+	 * @return
+	 */
+	public JSONObject performGetPaymentHistory(String userId, String startDate,
+			String endDate) {
+		return Helper.wrapUp(mAccSystem.getPaymentHistory(userId, startDate,
+				endDate));
+	}
+
+	// /**
+	// * 0117
+	// *
+	// * @author hosolo
+	// * @param userId
+	// * @return
+	// */
+	// public JSONObject performGetPaymentInfo(String userId) {
+	// return Helper.wrapUp(mAccSystem.getPaymentInfo(userId));
+	// }
+
+	/**
+	 * 0118
+	 * 
+	 * @author hosolo
+	 * @param userId
+	 * @return
+	 */
+	public JSONObject performGetPaymentName(String userId) {
+		return Helper.wrapUp(mAccSystem.getPaymentName(userId));
+	}
+
+	/**
+	 * 0119
+	 * 
+	 * @author hosolo
+	 * @return
+	 */
+	public JSONObject performGetPaymentNameOnMana() {
+		return Helper.wrapUp(mAccSystem.getPaymentNameOnMana());
+	}
+
+	/**
+	 * 0120
+	 * 
+	 * @author hosolo
+	 * @param userId
+	 * @return
+	 */
+	public JSONObject performGetPreAcc(String userId) {
+		return Helper.wrapUp(mAccSystem.getPreAcc(userId));
+	}
+
+	/**
+	 * 0121
+	 * 
+	 * @author hosolo
+	 * @return
+	 */
+	public JSONObject performGetSelServiceName() {
+		return Helper.wrapUp(mAccSystem.getSelServiceName());
+	}
+
+	/**
+	 * 0122
+	 * 
+	 * @author hosolo
+	 * @param userId
+	 * @return
+	 */
+	public JSONObject performGetSelServiceNameByUserId(String userId) {
+		return Helper.wrapUp(mAccSystem.getSelServiceNameByUserId(userId));
+	}
+
+	/**
+	 * 0123
+	 * 
+	 * @author hosolo
+	 * @param userId
+	 * @return
+	 */
+	public JSONObject performGetUsedCreditCard(String userId) {
+		return Helper.wrapUp(mAccSystem.getUsedCreditCard(userId));
+	}
+
+	/**
+	 * 0124
+	 * 
+	 * @author hosolo
+	 * @param userId
+	 * @param userPwd
+	 * @param exCode
+	 * @return
+	 */
+	public JSONObject performLogin(String userId, String userPwd, String exCode) {
+		return Helper.wrapUp(mAccSystem.login(userId, userPwd, exCode));
+	}
+
+	/**
+	 * 0125
+	 * 
+	 * @author hosolo
+	 * @param userId
+	 * @param accNo
+	 * @return
+	 */
+	public JSONObject performSetPreAcc(String userId, String accNo) {
+		return Helper.wrapUp(mAccSystem.setPreAcc(userId, accNo));
+	}
+
+	/**
+	 * 0126
+	 * 
+	 * @author hosolo
+	 * @param payName
+	 * @param state
+	 * @return
+	 */
+	public JSONObject performUpdatePaymentState(String payName, String state) {
+		return Helper.wrapUp(mAccSystem.updatePaymentState(payName, state));
+	}
+
 	/**
 	 * 
-	 * @author 王   亭
-	 * 2011-3-24
+	 * @author 王 亭 2011-3-24
 	 * @param account
 	 * @param password
 	 * @param amtph
@@ -87,16 +361,16 @@ public class Action {
 	 * @return
 	 */
 	public JSONObject performGetTargetPhoneInfo(String account,
-			String password, String amtph, double amtnum){
+			String password, String amtph, double amtnum) {
 		JSONObject jsonObj = new JSONObject();
-		jsonObj = Helper.wrapUp(mCurrentDepositt.getTargetPhoneInfo(account, password, amtph, amtnum));
+		jsonObj = Helper.wrapUp(mCurrentDepositt.getTargetPhoneInfo(account,
+				password, amtph, amtnum));
 		return jsonObj;
 	}
-	
+
 	/**
 	 * 
-	 * @author 王   亭
-	 * 2011-3-24
+	 * @author 王 亭 2011-3-24
 	 * @param account
 	 * @param password
 	 * @param amtph
@@ -104,69 +378,69 @@ public class Action {
 	 * @return
 	 */
 	public JSONObject performGetTransfeAct(String account, String password,
-			String amtph, double amtnum){
+			String amtph, double amtnum) {
 		JSONObject jsonObj = new JSONObject();
-		jsonObj = Helper.wrapUp(mCurrentDepositt.transfeAct(account, password, amtph, amtnum));
+		jsonObj = Helper.wrapUp(mCurrentDepositt.transfeAct(account, password,
+				amtph, amtnum));
 		return jsonObj;
 	}
-	
+
 	/**
 	 * 
-	 * @author 王   亭
-	 * 2011-3-24
+	 * @author 王 亭 2011-3-24
 	 * @param id
 	 * @return
 	 */
-	public JSONObject performGetComeQueryInfo(String id){
+	public JSONObject performGetComeQueryInfo(String id) {
 		JSONObject jsonObj = new JSONObject();
 		jsonObj = Helper.wrapUp(mCurrentDepositt.getComeQueryInfo(id));
 		return jsonObj;
 	}
-	
+
 	/**
 	 * 
-	 * @author 王   亭
-	 * 2011-3-24
+	 * @author 王 亭 2011-3-24
 	 * @param id
 	 * @return
 	 */
-	public JSONObject performGetListQueryInfo(String id){
+	public JSONObject performGetListQueryInfo(String id) {
 		JSONObject jsonObj = new JSONObject();
 		jsonObj = Helper.wrapUp(mCurrentDepositt.getListQueryInfo(id));
 		return jsonObj;
 	}
-	
+
 	/**
 	 * 
-	 * @author 王   亭
-	 * 2011-3-24
+	 * @author 王 亭 2011-3-24
 	 * @param serNo
 	 * @param detail
 	 * @return
 	 */
-	public JSONObject performSetDetail(String serNo, String detail){
+	public JSONObject performSetDetail(String serNo, String detail) {
 		JSONObject jsonObj = new JSONObject();
 		jsonObj = Helper.wrapUp(mCurrentDepositt.setDetail(serNo, detail));
 		return jsonObj;
 	}
-	
+
 	/**
 	 * 
-	 * @author 王   亭
-	 * 2011-3-24
+	 * @author 王 亭 2011-3-24
 	 * @param id
 	 * @param paymentNam
 	 * @return
 	 */
-	public JSONObject performGetPaymentHisInfo(String paymentNam,String id){
+	public JSONObject performGetPaymentHisInfo(String paymentNam, String id) {
+
 		JSONObject jsonObj = new JSONObject();
-		jsonObj = Helper.wrapUp(mCurrentDepositt.getPaymentHisInfo(paymentNam,id));
+
+		jsonObj = Helper.wrapUp(mCurrentDepositt.getPaymentHisInfo(paymentNam,
+				id));
 		return jsonObj;
 	}
+
 	/**
 	 * 
-	 * @author 王   亭
-	 * 2011-3-24
+	 * @author 王 亭 2011-3-24
 	 * @param paymentName
 	 * @param paymentAmt
 	 * @param paymentActNo
@@ -174,13 +448,14 @@ public class Action {
 	 * @param paymentNum
 	 * @return
 	 */
-	public JSONObject performRecharge(String paymentName,
-			double paymentAmt, String paymentActNo, String paymentActPasswd,String paymentNum){
+	public JSONObject performRecharge(String paymentName, double paymentAmt,
+			String paymentActNo, String paymentActPasswd, String paymentNum) {
 		JSONObject jsonObj = new JSONObject();
-		jsonObj = Helper.wrapUp(mCurrentDepositt.recharge(paymentName, paymentAmt, paymentActNo, paymentActPasswd, paymentNum));
+		jsonObj = Helper.wrapUp(mCurrentDepositt.recharge(paymentName,
+				paymentAmt, paymentActNo, paymentActPasswd, paymentNum));
 		return jsonObj;
 	}
-	
+
 	public JSONObject performGetPaymentInfo(String id) {
 		JSONObject jsonObj = new JSONObject();
 		jsonObj = Helper.wrapUp(mCurrentDepositt.getPaymentInfo(id));
